234bd1c92d30b76cd65613b1547068907983f879
Goran Belanovic <Goran Belanovic>
2021-01-19 17:09:33 +0100

    moving menu

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function Navigation({navVisible}) {
-    let navDisplay = navVisible? 'block' : 'none';
+    let navDisplay = navVisible? '0' : '-150%';
     return (
-        <nav className="navigation" style = {{display: navDisplay}}>
+        <nav 
+            className="navigation" 
+            style = {{
+                left: navDisplay
+               /*  transform: 'translateY(50%)' */
+            }}
+        >
             <ul className="nav-list">
                 <li className="nav-item"><a href={`#`} className="nav-link">Politika</a></li>
                 <li className="nav-item"><a href={`#`} className="nav-link">Svet</a></li>
