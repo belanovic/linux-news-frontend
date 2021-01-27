f5a0e13d5293f623d3c7d02528fcef32c25b0460
Goran Belanovic <goranbelanovic@yahoo.com>
2021-01-27 23:08:26 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,25 +4,23 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="container footer-container">
-                <div className = "footer-social">
-                <i class="fab fa-facebook-square"></i>
-                <i class="fab fa-twitter-square"></i>
-                <i class="fab fa-instagram-square"></i>
-                <i class="fab fa-linkedin"></i>
-                <i class="fab fa-youtube-square"></i>
+                <div className="footer-social">
+                    <i class="fab fa-facebook-square"></i>
+                    <i class="fab fa-twitter-square"></i>
+                    <i class="fab fa-instagram-square"></i>
+                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-youtube-square"></i>
                 </div>
-                <div className = "footer-links">
-                    <ul className = "footer-links-list">
-                        <li className = "footer-item"><a href = "#" className = "footer-item-link">O nama</a></li>
-                        <li className = "footer-item"><a href = "#" className = "footer-item-link">Impressum</a></li>
-                        <li className = "footer-item"><a href = "#" className = "footer-item-link">Marketing</a></li>
-                        <li className = "footer-item"><a href = "#" className = "footer-item-link">Kontakt</a></li>
-                    </ul>
-                </div>
-                <div className = "footer-copyright">
+                <ul className="footer-links">
+                    <li className="footer-item"><a href="#" className="footer-item-link">O nama</a></li>
+                    <li className="footer-item"><a href="#" className="footer-item-link">Impressum</a></li>
+                    <li className="footer-item"><a href="#" className="footer-item-link">Marketing</a></li>
+                    <li className="footer-item"><a href="#" className="footer-item-link">Kontakt</a></li>
+                </ul>
+                <div className="footer-copyright">
                     Copyright <i class="fas fa-copyright"> Goran Belanovic</i>
                 </div>
-                
+
             </div>
         </footer>
     )
