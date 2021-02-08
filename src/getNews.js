4ee165651c579a6ac39d4f78ee2fd4eade86d06d
Goran Belanovic <goranbelanovic@yahoo.com>
2021-02-08 02:49:26 +0100

    connected with heroku

--- src/getNews.js
+++ src/getNews.js
@@ -1,6 +1,8 @@
+import HOST_BACKEND from './hostBackend.js';
+
 export default async function getFrontpageNews() {
     try {
-        const response = await fetch('http://192.168.1.2:4000/frontpageArticles');
+        const response = await fetch(`http://${HOST_BACKEND}:4000/frontpageArticles`);
         const newsFrontpage = await response.json();
         return newsFrontpage
     }
