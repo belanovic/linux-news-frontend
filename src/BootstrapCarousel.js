06483e96bdef3857cb4bba152ee2ab48ab859be7
Goran Belanovic <Goran Belanovic>
2021-02-09 16:57:34 +0100

    with card in swipper

--- src/BootstrapCarousel.js
+++ src/BootstrapCarousel.js
@@ -1,44 +1,88 @@
-import React, {useState, useEffect} from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, Thumbs, Autoplay} from 'swiper';
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Thumbs, Autoplay } from 'swiper';
 import 'swiper/swiper-bundle.css';
+import Card from './Card.js';
 
 SwiperCore.use([Navigation, Pagination, Thumbs, Autoplay])
 
-export default function BootstrapCarousel({frontpageNews}) {
+export default function BootstrapCarousel({ frontpageNews }) {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  
-    return (
-          <>
-          <Swiper  
-            id = "main" 
-            autoplay = {{
-            delay: '3000'
-            }}
-            loop = {true} 
-            tag = 'section'
-            grabCursor = {true} 
-            wrapperTag = 'ul' 
-            navigation
-            thumbs = {{swiper: thumbsSwiper}}>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[0].imgURL} className = "imgSlide"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[1].imgURL} className = "imgSlide"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[2].imgURL} className = "imgSlide"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[3].imgURL} className = "imgSlide"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[4].imgURL} className = "imgSlide"></img></SwiperSlide>
-          </Swiper>
-          
-          <Swiper 
-            id = "thumbs" 
-            onSwiper = {setThumbsSwiper}
-            slidesPerView = {5} 
-            spaceBetween = {1}>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[0].imgURL} className = "imgThumb"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[1].imgURL} className = "imgThumb"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[2].imgURL} className = "imgThumb"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[3].imgURL} className = "imgThumb"></img></SwiperSlide>
-              <SwiperSlide tag = 'li'><img src = {frontpageNews[4].imgURL} className = "imgThumb"></img></SwiperSlide>
-          </Swiper>
-          </>
-        )
+
+  return (
+    <>
+      <Swiper
+        id="main"
+        autoplay={{
+          delay: '3000'
+        }}
+        loop={true}
+        tag='section'
+        grabCursor={true}
+        wrapperTag='ul'
+        navigation
+        thumbs={{ swiper: thumbsSwiper }}
+      >
+
+        <SwiperSlide tag='li'>
+          <Card
+            classSuffix='slide'
+            title={frontpageNews[0].title}
+            paragraph={frontpageNews[0].paragraph}
+            // date = {new Date().toLocaleDateString('rs-RS', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+            src={frontpageNews[0].imgURL}
+          />
+        </SwiperSlide>
+        <SwiperSlide tag='li'>
+          <Card
+            classSuffix='slide'
+            title={frontpageNews[1].title}
+            paragraph={frontpageNews[1].paragraph}
+            // date = {new Date().toLocaleDateString('rs-RS', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+            src={frontpageNews[1].imgURL}
+          />
+        </SwiperSlide>
+        <SwiperSlide tag='li'>
+          <Card
+            classSuffix='slide'
+            title={frontpageNews[2].title}
+            paragraph={frontpageNews[2].paragraph}
+            // date = {new Date().toLocaleDateString('rs-RS', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+            src={frontpageNews[2].imgURL}
+          />
+        </SwiperSlide>
+        <SwiperSlide tag='li'>
+          <Card
+            classSuffix='slide'
+            title={frontpageNews[3].title}
+            paragraph={frontpageNews[3].paragraph}
+            // date = {new Date().toLocaleDateString('rs-RS', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+            src={frontpageNews[3].imgURL}
+          />
+        </SwiperSlide>
+        <SwiperSlide tag='li'>
+          <Card
+            classSuffix='slide'
+            title={frontpageNews[4].title}
+            paragraph={frontpageNews[4].paragraph}
+            // date = {new Date().toLocaleDateString('rs-RS', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}
+            src={frontpageNews[4].imgURL}
+          />
+        </SwiperSlide>
+
+      </Swiper>
+
+      <Swiper
+        id="thumbs"
+        onSwiper={setThumbsSwiper}
+        slidesPerView={5}
+        spaceBetween={1}>
+        <SwiperSlide tag='li'><img src={frontpageNews[0].imgURL} className="imgThumb"></img></SwiperSlide>
+        <SwiperSlide tag='li'><img src={frontpageNews[1].imgURL} className="imgThumb"></img></SwiperSlide>
+        <SwiperSlide tag='li'><img src={frontpageNews[2].imgURL} className="imgThumb"></img></SwiperSlide>
+        <SwiperSlide tag='li'><img src={frontpageNews[3].imgURL} className="imgThumb"></img></SwiperSlide>
+        <SwiperSlide tag='li'><img src={frontpageNews[4].imgURL} className="imgThumb"></img></SwiperSlide>
+      </Swiper>
+    </>
+  )
 }
