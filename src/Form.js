97b8b1e67329454f704dbffe56c16af2fb4c7da0
Goran Belanovic <Goran Belanovic>
2021-04-27 18:44:34 +0200

    'msg'

--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,8 @@ import react, { useState, useEffect, useContext } from 'react';
 import cirilizatorDecirilizator from './cirilizatorDecirilizator.js';
 import { context } from './newsContext';
 import './styleForm.css';
+import {registerUser} from './getUser.js';
+import {loginUser} from './getUser.js';
 
 export default function Form() {
 
@@ -10,25 +12,45 @@ export default function Form() {
     const [signInisActive, setSignInIsActive] = useState(true);
     const [signUpisActive, setSignUpIsActive] = useState(false);
 
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('');
+    const [usernameSignIn, setUsernameSignIn] = useState('');
+    const [usernameSignUp, setUsernameSignUp] = useState('');
+    const [passwordSignUp, setPasswordSignUp] = useState('');
+    const [passwordSignIn, setPasswordSignIn] = useState('');
+    const [email, setEmail] = useState('');
 
-    const handleClickSend = (e) => {
-        e.preventDefault()
+
+    const handleChange = (e, setFunc) => {
+        const value = e.target.value;
+        setFunc(value);
     }
+
     const handleClickTab = (tab) => {
         if (tab === 'sign-up') {
             setSignUpIsActive(true);
             setSignInIsActive(false);
-        } else if(tab === 'sign-in') {
+        } else if (tab === 'sign-in') {
             setSignUpIsActive(false);
             setSignInIsActive(true);
         }        
     }
 
+    const handleClickSignIn = async (e) => {
+        e.preventDefault();
+        const user = await loginUser(usernameSignIn, passwordSignIn);
+        console.log(user);
+    }
+    
+    const handleClickSignUp = async (e) => {
+        e.preventDefault();
+        const newUser = await registerUser(firstName, lastName, usernameSignUp, passwordSignUp, email);
+        console.log(newUser);
+    }
+    
     return (
         <>
             <div className="form-container">
-               {/*  <div className="form-around2">
-                    <div className="form-around1"> */}
                         <form className="form">
                             <div className="form-title">
                                 <span 
@@ -46,7 +68,13 @@ export default function Form() {
                                 style = {{display: signUpisActive? 'block' : 'none'}}
                             >
                                 <label htmlFor="firstName" className="firstName-label">{cirilizatorDecirilizator(alphabet, 'Ime')}</label>
-                                <input id="firstName" className="firstName-input" type="text"></input>
+                                <input 
+                                    id="firstName" 
+                                    className="firstName-input" 
+                                    type="text"
+                                    value = {firstName}
+                                    onChange = {(e) => handleChange(e, setFirstName)}
+                                ></input>
                             </div>
 
                             <div 
@@ -54,17 +82,67 @@ export default function Form() {
                                 style = {{display: signUpisActive? 'block' : 'none'}}
                             >
                                 <label htmlFor="lastName" className="lastName-label">{cirilizatorDecirilizator(alphabet, 'Prezime')}</label>
-                                <input id="lastName" className="lastName-input" type="text" ></input>
+                                <input 
+                                    id="lastName" 
+                                    className="lastName-input" 
+                                    type="text" 
+                                    value = {lastName}
+                                    onChange = {(e) => handleChange(e, setLastName)}
+                                ></input>
                             </div>
 
-                            <div className="form-element username">
-                                <label htmlFor="username" className="username-label">{cirilizatorDecirilizator(alphabet, 'Korisničko ime')}</label>
-                                <input id="username" className="username-input" type="text"></input>
+                            <div 
+                                className="form-element usernameSignIn"
+                                style = {{display: signInisActive? 'block' : 'none'}}
+                            >
+                                <label htmlFor="usernameSignIn" className="usernameSignIn-label">{cirilizatorDecirilizator(alphabet, 'Korisničko ime')}</label>
+                                <input 
+                                    id="usernameSignIn" 
+                                    className="usernameSignIn-input" 
+                                    type="text"
+                                    value = {usernameSignIn}
+                                    onChange = {(e) => handleChange(e, setUsernameSignIn)}
+                                ></input>
+                            </div>
+                            <div 
+                                className="form-element usernameSignUp"
+                                style = {{display: signUpisActive? 'block' : 'none'}}
+                            >
+                                <label htmlFor="usernameSignUp" className="usernameSignUp-label">{cirilizatorDecirilizator(alphabet, 'Korisničko ime')}</label>
+                                <input 
+                                    id="usernameSignUp" 
+                                    className="usernameSignUp-input" 
+                                    type="text"
+                                    value = {usernameSignUp}
+                                    onChange = {(e) => handleChange(e, setUsernameSignUp)}
+                                ></input>
                             </div>
 
-                            <div className="form-element pasword">
-                                <label htmlFor="password" className="pasword-label">{cirilizatorDecirilizator(alphabet, 'Šifra')}</label>
-                                <input id="username" className="pasword-input" type="text" ></input>
+                            <div 
+                                className="form-element paswordSignIn"
+                                style = {{display: signInisActive? 'block' : 'none'}}
+                            >
+                                <label htmlFor="passwordSignIn" className="pasword-label">{cirilizatorDecirilizator(alphabet, 'Šifra')}</label>
+                                <input 
+                                    id="passwordSignIn" 
+                                    className="paswordSignIn-input" 
+                                    type="text" 
+                                    value = {passwordSignIn}
+                                    onChange = {(e) => handleChange(e, setPasswordSignIn)}
+                                ></input>
+                            </div>
+                            <div 
+                                className="form-element paswordSignUp"
+                                style = {{display: signUpisActive? 'block' : 'none'}}
+                            >
+                                <label htmlFor="passwordSignUp" className="paswordSignUp-label">{cirilizatorDecirilizator(alphabet, 'Šifra')}</label>
+                                <input 
+                                    id="passwordSignUp" 
+                                    className="paswordSignUp-input" 
+                                    type="text" 
+                                    value = {passwordSignUp}
+                                    onChange = {(e) => handleChange(e, setPasswordSignUp)}
+                                ></input>
                             </div>
 
                             <div 
@@ -72,17 +150,21 @@ export default function Form() {
                                 style = {{display: signUpisActive? 'block' : 'none'}}
                             >
                                 <label htmlFor="username" className="email-label">e-mail</label>
-                                <input id="username" className="email-input" type="email"></input>
+                                <input 
+                                    id="username" 
+                                    className="email-input" 
+                                    type="email"
+                                    value = {email}
+                                    onChange = {(e) => handleChange(e, setEmail)}
+                                ></input>
                             </div>
                             <div className="form-send">
                                 <button 
                                     className="form-send-button"
-                                    onClick = {handleClickSend}
+                                    onClick = {signInisActive? handleClickSignIn : handleClickSignUp}
                                 >{cirilizatorDecirilizator(alphabet, `${signInisActive? 'Prijavi se' : 'Registruj se'}`)}</button>
                             </div>
                         </form>
-                   {/*  </div>
-                </div> */}
             </div>
         </>
     )
