7eec62250f3f79b6824c54a1ae4108ae87b4dff9
Goran Belanovic <goranbelanovic@yahoo.com>
2022-01-12 16:26:54 +0100

    'msg'

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ import debounce from './debounce.js';
 import shortenSentence from './shortenSentence.js';
 import GenericThumb from './GenericThumb';
 import './style/layout/card.css';
-import './style/typography/card.css';
+import './style/typography/card.css';  
 
 export default function Card({ position, classSuffix, title, subtitle, paragraphs, videoURL,
     datePublished, dateUpdated, src, filter, id, cathegory, frontpageNews, thumbShape }) {
@@ -17,6 +17,7 @@ export default function Card({ position, classSuffix, title, subtitle, paragraph
 
     const [filterStyle, setFilterStyle] = useState('none');
 
+    
     useEffect(() => {
         if (filter) {
             setFilterStyle(() => {
