1739a60e3a755355be2d64e5f5229dfb0a87284c
Goran Belanovic <goranbelanovic@yahoo.com>
2024-06-21 03:08:44 +0200

    msg

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import {Provider} from './newsContext.js';
 import './style/layout/layout.css';
 import './style/typography/typography.css';
 import './style/alphabet.css';
+import './style/variables.css';
 
 ReactDom.render(<Provider><Router><App /></Router></Provider>, document.getElementById('root'));
